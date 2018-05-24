0b1cc578ca836b611f2bdef9f64e33c08ffb0b38
= <=>
2018-05-23 21:16:50 -0300

    'Funcionando'

--- topico-6/atividade4.js
+++ topico-6/atividade4.js
@@ -1,19 +1,24 @@
-//Função de módulo
+
 
 let varX = parseInt(process.argv[2]);
 let varY = parseInt(process.argv[3]);
 
 let mod = function (x, y){
     
-    if (y <= 0) ||  (x < 0 ){
+    console.log('Na função');
+
+    if (y <= 0 || x < 0 ){
         return -1;
+        console.log('Entrou');
     }
 
-    for (let s = x, y <= s, s++){
+    let s = x
+
+    for (; y <= s; s++){
         s = s - y;
     }
 
-    return s;
-
-
+ return s;
 }
+
+console.log(mod(varX,varY));
