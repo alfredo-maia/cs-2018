d3217aafdef4f1fc187e9f1607a02fd28a5b64a0
= <=>
2018-06-27 19:36:50 -0300

    Adcionando Repositório

---
+++ topico-8/classes.js
@@ -0,0 +1,45 @@
+/**
+ * Armazena todas as classe do projeto
+ * 
+ * Cada classe contém uma atividade dos requistos
+ * 
+ * @author Alfredo Souza Maia
+ * @copyright Todos direitos reservados 
+ * @version 1.0
+ * 
+ * <p>Adicionalmente, a presente classe também ilustra o uso de
+ * <a href="http://usejsdoc.org/">JSDoc</a>.</p>
+ *
+ * @type {module.Fatorial}
+ */
+export default class Fatorial {
+
+    /* global console, process,exports */
+
+
+
+    /** 
+     * @function Retorna o fatorial de um número
+     * @param {number} num - número inteiro de entrada
+     * @example Entre com o número 5
+     * @returns Retorna o valor do fatorial
+    */
+
+    function fat(num) {
+    
+    /**@type {number}*/
+    let i = 2;
+    /**@type {number}*/
+    let f = 1;
+    
+        while (i <= fat) {
+            f = f * i;
+            i = i + 1;
+        }
+
+        /**@returns Retorno da função */
+        return f;
+
+}
+
+}
---
+++ topico-8/main.js
@@ -0,0 +1,7 @@
+/ ES6
+
+import Fatorial from "./classes";
+
+const fatorial = new ();
+
+console.log(funcoes.soma(1,2) + funcoes.subtracao(1,2));
